798eb128fcb7032a2c2844cb65a6b7e79f9cc47f
ChinaLike <572919350@qq.com>
2019-07-03 16:18:11 +0800

    1.删除多余数据

--- app/navigator/TabNavigator.js
+++ app/navigator/TabNavigator.js
@@ -53,59 +53,12 @@ const TabNavigator = createBottomTabNavigator(
   }
 )
 
-/**
- * 设置导航栏样式
- * @param {*} param0
- */
-const navigationOptions = ({ navigation }) => {
-  let title = ''
-  const index = navigation.state.index
-  if (index == 0) {
-    title = '首页'
-  } else if (index == 1) {
-    title = '通讯录'
-  } else if (index == 2) {
-    title = '待办'
-  } else if (index == 3) {
-    title = '我的'
-  }
-  return {
-    title,
-    gesturesEnabled: true,
-    headerTitleStyle: {
-      fontSize: 18,
-      flex: 1,
-      textAlign: 'center'
-    },
-    headerTransparent: false,
-    // headerBackground: (
-    //   <Image
-    //     source={require('../res/images/tab/navigation_bg.png')}
-    //     style={{ flex: 1 }}
-    //   />
-    // ),
-    headerStyle: {
-      ...Platform.select({
-        android: {
-          paddingTop: StatusBar.currentHeight,
-          height: 50 + StatusBar.currentHeight
-        }
-      })
-    },
-    headerPressColorAndroid: 'rgba(0,0,0,0.2)'
-  }
-}
-
 TabNavigator.navigationOptions = ({ navigation }) => {
   let title = ''
   const index = navigation.state.index
   if (index == 0) {
     title = '首页'
   } else if (index == 1) {
-    title = '通讯录'
-  } else if (index == 2) {
-    title = '待办'
-  } else if (index == 3) {
     title = '我的'
   }
   return Config.tabNavigationConfig(
